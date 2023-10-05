85338a8e16ba09edf3f042c98016d903b7db8ce4
gabelee <gabennlee@gmail.com>
2023-10-04 21:16:14 -0700

    tests

--- main.rb
+++ main.rb
@@ -1,1 +1,11 @@
+class Player
+    attr_reader :name, :score
+    attr_writer :name, :score
+    attr_accessor :name, :score  #read/write
+
+    p = Player.new("name", score)
+    put p.name
+
+    (1..10).each {|i| puts i}
+end
 
